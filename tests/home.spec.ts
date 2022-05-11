d29166c5e400d42f855ddd78b43ba0afbfc8a72d
Andy Barnes <andrew.barnes102@googlemail.com>
2022-05-11 18:01:39 +0100

    Moved Playwright onto its own port, so it can run in parallel when developing

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -2,7 +2,7 @@ import { test } from '@playwright/test';
 import { injectAxe, checkA11y } from 'axe-playwright';
 
 test('accessibility test', async ({ page }) => {
-  await page.goto('http://localhost:3000');
+  await page.goto('/');
 
   // accessibility check
   await injectAxe(page);
